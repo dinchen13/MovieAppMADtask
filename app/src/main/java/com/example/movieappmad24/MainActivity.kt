f0afdceaa57bc38c6a299b9624981fc3f58bd0dd
LAPTOP-4HEA5EIP\nadin <nadine.kirnbauer@stud.fh-campuswien.ac.at>
2024-02-29 23:47:24 +0100

    refactor (task 2.4)

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -26,22 +24,17 @@ import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ShapeDefaults
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -49,11 +42,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.ModifierInfo
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -62,7 +52,6 @@ import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)     //das braucht man für die topbar, macht der typ im youtubevideo auch (von den bereitgestellten resourcen)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -73,61 +62,72 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val scrollbehavior = TopAppBarDefaults.pinnedScrollBehavior() //das ist wie sich die top bar beim scrollen verhalten soll zb (enterAlwaysScrollBehavior()) lässt die bar beim runterswipen verschwinden
-                    Scaffold(
-                        modifier= Modifier
-                            .fillMaxSize()
-                            .nestedScroll(scrollbehavior.nestedScrollConnection),  //optional
-                        topBar = {
-                            CenterAlignedTopAppBar(title = {
-                                Text("Movie App")
-                            },
-                                scrollBehavior = scrollbehavior, //optional
-                                colors= TopAppBarDefaults.topAppBarColors(
-                                    containerColor = MaterialTheme.colorScheme.onBackground,  //hab das hinzugefügt zu Theme.kt damit ich dafür auch eine dynamische farbe hab
-                                    titleContentColor = MaterialTheme.colorScheme.primary,
-                                )
-                            )
-                        },
-                        bottomBar = {
-                            BottomAppBar(
-                                containerColor = MaterialTheme.colorScheme.background,
-                                contentColor = MaterialTheme.colorScheme.primary,
-                            ) {
-                                var watchlistSelected by remember { mutableStateOf(false) }
-                                NavigationBarItem( selected = !watchlistSelected,
-                                    onClick = {watchlistSelected=!watchlistSelected},
-                                    label = {Text(text = "Home")},
-                                    icon = {Icon(
-                                        imageVector = Icons.Default.Home,
-                                        contentDescription = "Home"
-                                    )}
-                                )
-                                NavigationBarItem( selected = watchlistSelected,
-                                    onClick = {watchlistSelected=!watchlistSelected},
-                                    label = {Text(text = "Watchlist")},
-                                    icon = {Icon(
-                                        imageVector = Icons.Default.Star,
-                                        contentDescription = "Watchlist"
-                                    )}
-                                )
-                            }
-                        },
-
-
-                        ) {
-                            innerPadding ->
-                        Column(
-                            modifier = Modifier
-                                .padding(innerPadding),
-                            verticalArrangement = Arrangement.spacedBy(16.dp),
-                        ) {
-                            MovieList(movies = getMovies())
-                        }
-                    }
+                    HomeScaffold()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun HomeScaffold(){
+    Scaffold(
+        modifier= Modifier
+            .fillMaxSize(),
+        topBar = {
+            HomeTopBar()
+        },
+        bottomBar = {
+            HomeBottomBar()
+        },
+        ) {
+            innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            MovieList(movies = getMovies())
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)     //das braucht man für die topbar, macht der typ im youtubevideo auch (von den bereitgestellten resourcen)
+fun HomeTopBar() {      //eigenen funktion mainly for readability
+    CenterAlignedTopAppBar(
+        title = {
+            Text("Movie App")
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.onBackground,  //hab das hinzugefügt zu Theme.kt damit ich dafür auch eine dynamische farbe hab
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        )
+    )
+}
+@Composable
+fun HomeBottomBar(){    //eigenen funktion mainly for readability
+    BottomAppBar(
+        containerColor = MaterialTheme.colorScheme.background,
+        contentColor = MaterialTheme.colorScheme.primary,
+    ) {
+        var watchlistSelected by remember { mutableStateOf(false) }
+        NavigationBarItem( selected = !watchlistSelected,
+            onClick = {watchlistSelected=!watchlistSelected},
+            label = {Text(text = "Home")},
+            icon = {Icon(
+                imageVector = Icons.Default.Home,
+                contentDescription = "Home"
+            )}
+        )
+        NavigationBarItem( selected = watchlistSelected,
+            onClick = {watchlistSelected=!watchlistSelected},
+            label = {Text(text = "Watchlist")},
+            icon = {Icon(
+                imageVector = Icons.Default.Star,
+                contentDescription = "Watchlist"
+            )}
+        )
     }
 }
 
@@ -140,13 +140,11 @@ fun MovieList(movies: List<Movie> = getMovies()){
     }
 }
 
-
 @Composable
 fun MovieRow(movie: Movie){
     var showDetails by remember {
         mutableStateOf(false)
     }
-
     Card(modifier = Modifier
         .fillMaxWidth()
         .padding(5.dp),
@@ -160,20 +158,8 @@ fun MovieRow(movie: Movie){
                     .fillMaxWidth(),
                 contentAlignment = Alignment.Center
             ) {
-                AsyncImage(
-                    modifier = Modifier.fillMaxSize(),
-                    model = movie.images[1],
-                    contentDescription = null,
-                    placeholder = painterResource(id = R.drawable.movie_image),
-                    contentScale = ContentScale.Crop
-                )
-/*
-                Image(
-                    painter = painterResource(id = R.drawable.movie_image),
-                    contentScale = ContentScale.Crop,
-                    contentDescription = "placeholder image")
- */
-                Box(
+                HttpImageOfMovie(movie.images[1], "image of " + movie.title)
+                Box(            //fürs Plazieren vom Icon
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(10.dp),
@@ -185,7 +171,6 @@ fun MovieRow(movie: Movie){
                         contentDescription = "Add to favorites")
                 }
             }
-            
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -204,18 +189,34 @@ fun MovieRow(movie: Movie){
             }
             //Text(text = movie.plot, maxLines = if (showDetails) Int.MAX_VALUE else 1)   //das wär eine zweite option statt andimatedvisability dazu braucht man aber noch irgendwo ein .animateContentSize() beim Modifier
             AnimatedVisibility(showDetails) {
-                Column(Modifier.padding(15.dp)) {
-                    Text(text = "Director: " + movie.director)
-                    Text(text = "Released: " + movie.year)
-                    Text(text = "Genre: " + movie.genre)
-                    Text(text = "Actors: " + movie.actors)
-                    Text(text = "Rating: " + movie.rating)
-                    Divider()
-                    Text(text = "Plot: " + movie.plot)
-                }
+                MovieInfo(movie)
             }
         }
     }
+}
+
+@Composable
+fun MovieInfo(movie:Movie){     //eigene funktion weil ich vielleicht später mal wieder die ganzen infos gesammelt brauch
+    Column(Modifier.padding(15.dp)) {
+        Text(text = "Director: " + movie.director)
+        Text(text = "Released: " + movie.year)
+        Text(text = "Genre: " + movie.genre)
+        Text(text = "Actors: " + movie.actors)
+        Text(text = "Rating: " + movie.rating)
+        Divider(Modifier.padding(12.dp))
+        Text(text = "Plot: " + movie.plot)
+    }
+}
+
+@Composable
+fun HttpImageOfMovie(httpSource:String, description:String?){      //eigenen funktion wenn ich sonst nochmal irgenwo bilder von http source schnell anzeigen will
+    AsyncImage(
+        modifier = Modifier.fillMaxSize(),
+        model = httpSource,    //because there are multiple pictures provided, i take the first one
+        contentDescription = description?:"no description available",
+        placeholder = painterResource(id = R.drawable.movie_image),
+        contentScale = ContentScale.Crop
+    )
 }
 
 @Preview
